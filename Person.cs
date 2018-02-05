0bced1e40896e50eab11076263720675bc02822e
DanE <edwardsdan001@gmail.com>
2018-02-05 13:21:10 -0500

    initial commit

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Person
+    {
+        private string name;
+        private string address;
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Person()
+        {
+            Name = "not provided";
+            Address = "not provided";
+        }
+
+        public Person(string pName, string pAddress)
+        {
+            Name = pName;
+            Address = pAddress;
+        }
+
+        public override string ToString()
+        {
+            return $"This person's name is {Name} and their address is {Address}";
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Student : Person
+    {
+        private string program;
+        private int year;
+        private double fee;
+
+        public string Program {get; set;}
+        public int Year { get; set; }
+        public double Fee { get; set; }
+
+        public Student(string name, string address, string program, int year, double fee):base(name, address)
+        {
+            Program = program;
+            Year = year;
+            Fee = fee;
+        }
+
+        public Student()
+        {
+            Name = "not provided";
+            Address = "not provided";
+            Program = "not provided";
+            Year = 0;
+            Fee = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Student! Name: {Name} Address: {Address} Program: {Program} Year: {Year} Fee: {Fee}";
+        }
+    }
+}
